a2c4aac2d4f935b936138c65b9d1a36542de0d1c
Parth Sharma <99831413+PsVenom@users.noreply.github.com>
2022-07-20 15:53:53 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,22 +1,29 @@
 
 from transformers import AutoTokenizer, AutoModel,pipeline
 from transformers import BartTokenizer, BartModel
+from transformers import CLIPProcessor, CLIPModel
+
 import numpy as np
 from sklearn.decomposition import PCA
 import subprocess
 
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+summarize = pipeline("summarization", model = "facebook/bart-large")
+tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large")
+model = BartModel.from_pretrained('facebook/bart-large')
+pca =  PCA(n_components = 3)
+
 def BART_t2t(text):
- summarize = pipeline("summarization", model = "facebook/bart-large")
  return summarize(text, min_length = 3,max_length = 20, do_sample = False)
 
 def BART_pl(text):
-  tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large")
-  model = BartModel.from_pretrained('facebook/bart-large')
   text = BART_t2t(text)[0]['summary_text']
   inputs = tokenizer(text, return_tensors="pt")
   return model(**inputs)
 
-pca =  PCA(n_components = 3)
+
+
 def reduce_channels(encoding): #dimensionality reduction using PCA and mean correction
  array = encoding.detach().cpu().numpy().reshape(-1,1024).T
  X_new = array - np.mean(array)
@@ -28,3 +35,9 @@ def text2PCA(word):
 #now we can finally convert this array to (32,32,3) image array
 def t2i(word):
   return text2PCA(word).reshape(32,32,3)
+ 
+def L_clip(Xt,Yi): #Xt is a string, Yi is an image array
+  inputs = processor(text= Xt, images= Yi, return_tensors="pt", padding=True)
+  outputs = model(**inputs)
+  logits_per_image = outputs.logits_per_image
+  return 1 - (logits_per_image/100) #returns a pytorch tensor
