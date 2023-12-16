0689ef251a267f0ba9bd7153629c64b0c1320043
PsVenom <luckyparth1401@gmail.com>
2023-12-16 18:39:36 +0530

    fixing train_seq

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,8 +1,8 @@
 import numpy as np
 import click
 from .predict_model import call_model_for_train
-from data.make_dataset import create_dataloader
-from helper import plot_images
+from src.data.make_dataset import create_dataloader
+from .helper import plot_images
 import os
 import tensorflow as tf
 import keras
@@ -11,8 +11,10 @@ batch_sizes = {2: 16, 3: 16, 4: 16, 5: 16, 6: 16, 7: 8, 8: 4, 9: 2, 10: 1}
 train_step_ratio = {k: batch_sizes[2] / v for k, v in batch_sizes.items()}
 
 def train_seq(
+        cv, iv,
         start_res=4,
         target_res=128,
+
         steps_per_epoch=2000,
         style_gan = call_model_for_train(START_RES=4, TARGET_RES=64, BETA=0.99, gen_per_epoch=1,weight_dir=None)
 ):
@@ -30,7 +32,7 @@ def train_seq(
             if res == start_res and phase == "TRANSITION":
                 continue
 
-            train_dl, caption_dl = create_dataloader(res)
+            train_dl, caption_dl = create_dataloader(res, iv, cv)
             dataset = tf.data.Dataset.zip((train_dl, caption_dl))
 
             steps = int(train_step_ratio[res_log2] * steps_per_epoch) if phase == 'TRANSITION' else int(train_step_ratio[res_log2] * steps_per_epoch)/2
--- train.py
+++ train.py
@@ -6,16 +6,16 @@ from src.data.make_dataset import make_dataset
 import os
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True),  default = os.get_cwd())
-@click.argument('START_RES', type = click.INT, default = 4, help = 'START_RESOLUTION for base model, default is 4' )
-@click.argument('TARGET_RES', type = click.INT, default = 128, help = 'TARGET_RESOLUTION for base, default is 128' )
-@click.argument('STEPS', type = click.INT, default = 2000, help = 'STEPS for base model training, default is 2000' )
-@click.argument('WEIGHTS', type = click.STRING, default = None, help = 'WEIGHTS for base model training, default is None' )
-@click.argument('SHOW_IMG', type = click.BOOL, default = False,help ='shows image for base model training')
-def train(input_filepath,start_res, target_res, steps, weights, see_img = False):
+@click.option('-s', type = click.INT, default = 4, help = 'START_RESOLUTION for base model, default is 4' )
+@click.option('-t', type = click.INT, default = 128, help = 'TARGET_RESOLUTION for base, default is 128' )
+@click.option('-step',  type = click.INT, default = 2000, help = 'STEPS for base model training, default is 2000' )
+@click.option('-w',  type = click.STRING, default = None, help = 'WEIGHTS for base model training, default is None' )
+@click.option('-show', type = click.BOOL, default = False,help ='shows image for base model training')
+def train(input_filepath,s, t, step, w, show = False):
     cap_vector, img_vectors = make_dataset(input_filepath)
     tf.config.run_functions_eagerly(True)
-    style_gan =  call_model_for_train(START_RES=start_res, TARGET_RES=target_res, BETA=0.99, gen_per_epoch=1,weight_dir=weights)
-    train_seq(start_res=start_res, target_res=target_res, display_images=False, steps_per_epocj = steps, style_gan=style_gan)
+    style_gan =  call_model_for_train(START_RES=s, TARGET_RES=t, BETA=0.99, gen_per_epoch=1,weight_dir=w)
+    train_seq(start_res=s, target_res=t, cv = cap_vector, iv = img_vectors, steps_per_epoch = step, style_gan=style_gan)
 
 if __name__ == "__main__":
     train()
